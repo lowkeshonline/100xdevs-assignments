52513a94d546ee192786170efbac538fab33002f
lowkeshonline <lowkeshonline@gmail.com>
2025-02-26 19:14:38 +0530

    updated JS into cleaner code

--- DOM/todo_app/script.js
+++ DOM/todo_app/script.js
@@ -3,16 +3,26 @@
 let ctr = 1;
 
 function deleteTask(index){   //Delete button call
-    console.log("delete task called");
-    const deleteEl = document.getElementById(index);
-    deleteEl.parentElement.removeChild(deleteEl);
+    console.log('Delete called');
+    const delEl = document.querySelector('#task-' + index);
+    delEl.parentElement.removeChild(delEl);
 }
 
 function addTask(){
-    const inputText = document.querySelector('input').value;
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute('id', ctr);
-    newDiv.innerHTML = "<div>" + inputText + "</div><button onclick='deleteTask(" + ctr + ")'> Delete </button>";
-    document.querySelector('#tasks').appendChild(newDiv);
-    ctr = ctr + 1;
+    const inputValue = document.querySelector('input').value;
+
+    const todoText = document.createElement('span');
+    const delButton = document.createElement('button');
+
+    todoText.innerHTML = inputValue;
+    delButton.innerHTML = "Delete";
+    delButton.setAttribute('onclick', "deleteTask(" + ctr + ")");
+
+    const divEl = document.createElement('div');
+    divEl.setAttribute('id', 'task-' + ctr);
+    divEl.appendChild(todoText);
+    divEl.appendChild(delButton);
+
+    document.getElementById("tasks").appendChild(divEl);
+    ctr = ctr+1;
 }
